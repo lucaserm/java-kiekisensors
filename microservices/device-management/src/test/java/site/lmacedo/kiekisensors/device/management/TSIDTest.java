6bc06b7313d9f6f0a12390d52cce5f9819dd06cc
lucaserm <lucasedumacedo@gmail.com>
2025-08-01 10:25:08 -0400

    feat: implementing UUIDv7 and TSID

---
+++ microservices/device-management/src/test/java/site/lmacedo/kiekisensors/device/management/TSIDTest.java
@@ -0,0 +1,18 @@
+package site.lmacedo.kiekisensors.device.management;
+
+import io.hypersistence.tsid.TSID;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+class TSIDTest {
+
+    @Test
+    void shouldGenerateTSID() {
+        TSID tsid = IdGenerator.generateTSID();
+        Assertions.assertThat(tsid.getInstant())
+                .isCloseTo(Instant.now(), Assertions.within(1, ChronoUnit.MINUTES));
+    }
+}
---
+++ microservices/temperature-processing/src/test/java/site/lmacedo/kiekisensors/temperature/processing/UUIDv7Test.java
@@ -0,0 +1,19 @@
+package site.lmacedo.kiekisensors.temperature.processing;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+class UUIDv7Test {
+
+    @Test
+    void shouldGenerateUUIDv7(){
+        UUID uuid = IdGenerator.generateTimeBasedUUID();
+        OffsetDateTime uuidDateTime = UUIDv7Utils.extractOffsetDateTime(uuid).truncatedTo(ChronoUnit.MINUTES);
+        OffsetDateTime currentDateTime = OffsetDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+        Assertions.assertThat(uuidDateTime).isEqualTo(currentDateTime);
+    }
+}
