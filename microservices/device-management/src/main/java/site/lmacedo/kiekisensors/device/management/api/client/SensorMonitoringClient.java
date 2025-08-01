fcef3a51f3d43480074465dd05bc0424d29ac6fc
lucaserm <lucasedumacedo@gmail.com>
2025-08-01 17:23:57 -0400

    feat: setting up a best config to rest client

--- microservices/device-management/src/main/java/site/lmacedo/kiekisensors/device/management/api/client/SensorMonitoringClient.java
+++ microservices/device-management/src/main/java/site/lmacedo/kiekisensors/device/management/api/client/SensorMonitoringClient.java
@@ -1,10 +1,21 @@
 package site.lmacedo.kiekisensors.device.management.api.client;
 
 import io.hypersistence.tsid.TSID;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.service.annotation.DeleteExchange;
+import org.springframework.web.service.annotation.GetExchange;
+import org.springframework.web.service.annotation.HttpExchange;
 import site.lmacedo.kiekisensors.device.management.api.model.SensorMonitoringOutput;
 
+@HttpExchange("/api/sensors/{sensorId}/monitoring")
 public interface SensorMonitoringClient {
-    void enableMonitoring(TSID sensorId);
-    void disableMonitoring(TSID sensorId);
-    SensorMonitoringOutput getDetail(TSID sensorId);
+    @PutMapping("/enable")
+    void enableMonitoring(@PathVariable TSID sensorId);
+
+    @DeleteExchange("/enable")
+    void disableMonitoring(@PathVariable TSID sensorId);
+
+    @GetExchange
+    SensorMonitoringOutput getDetail(@PathVariable TSID sensorId);
 }
